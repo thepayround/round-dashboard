984a479c5b5e9146622580df206a9f494f7868a8
Dimitris Petas <petasjim@gmail.com>
2025-09-14 00:56:06 +0300

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,7 @@ const App = () => {
         />
         <Route path="/" element={<HomePage />} />
       </Routes>
-        </Router>
+      </Router>
       </AuthProvider>
     </ToastProvider>
   )
